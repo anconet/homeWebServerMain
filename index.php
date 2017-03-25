336b80b522a6419005fc8cabf8c35681cfa50d18
Anthony Anconetani <anthony.anconetani@gmail.com>
2017-03-25 16:29:49 -0500

    Cleaned up the IP Table a bit

--- index.php
+++ index.php
@@ -1,23 +1,27 @@
 <html>
     <head>
-        <meta charset="utf-8">
-        <title>Project: Travel webpage</title>
-        
-        <style>
-            
-            #italy{
-                color:green;
-            }
-            
-            body{
-                background:rgb(225,225,200);
-            }
-            
-            h3{
-                color:rgb(227, 127, 227);
+	<meta charset="utf-8">
+	<title>Project: Travel webpage</title>
+
+	<style>
+
+	    #italy{
+		color:green;
 	    }
-        </style>
-        
+
+	    body{
+		background:rgb(225,225,200);
+	    }
+
+	    h3{
+		color:rgb(227, 127, 227);
+	    }
+
+th, td {
+text-align: center;
+}
+	</style>
+
     </head>
 <body><h1>It works!</h1>
 <p>This is the default web page for this server.</p>
@@ -31,7 +35,12 @@
 echo"";
 echo "<h1>DNS Table</h1><br>";
 $fileContents = file("/var/lib/misc/dnsmasq.leases");
+echo "<table>";
+echo "<tr><th>IP Address</th><th>Host</th></tr>";
 foreach ($fileContents as $line) {
-	echo "<p>$line</p>";
+	preg_match("/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) ([\w-]+) /",$line,$output_array);
+	echo "<tr><td>$output_array[1]</td><td>$output_array[2]</td></tr>";
+	//	echo "<p>$line</p>";
 }
+echo "</table>";
 ?>
